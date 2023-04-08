4041f602c1f00ae6e1a8797bbd2e2d4d3d1d9d02
juanca502 <jvicentepablo@gmail.com>
2023-04-08 15:04:01 -0600

    video 05 Grupos de Rutas en laravel 10

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,11 +16,17 @@ use App\Http\Controllers\CursoController;
 */
 
 Route::get('/', HomeController::class);
+/**grupos de Rutas */
+Route::controller(CursoController::class)->group(function(){
+    /**Metodos */
+    Route::get('cursos',  'index');
+    Route::get('cursos/create','create');
+    Route::get('cursos/{curso}','show');
 
 
-Route::get('cursos', [CursoController::class, 'index']);
-Route::get('cursos/create', [CursoController::class, 'create']);
-Route::get('cursos/{curso}',[CursoController::class, 'show']);
+});
+
+
 
    /* Route::get('cursos/{curso}/{categoria?}' , function ($curso, $categoria= null){
         if($categoria){
